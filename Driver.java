a60794e1e9d53ebcf014de12f486dadf499a3d88
yoshixi <davidchenyuhe@proton.me>
2025-05-26 14:44:30 -0400

    modified Polynomial.java and Driver.java'

--- Driver.java
+++ Driver.java
@@ -1,27 +1,24 @@
+import java.io.File;
+
 public class Driver {
   public static void main(String [] args) {
     Polynomial p = new Polynomial();
     System.out.println(p.evaluate(3));
     double [] cof1 = {6,2,5};
-    double [] pow1 = {0,1,3};
+    int [] pow1 = {0,1,3};
     Polynomial p1 = new Polynomial(cof1,pow1);
     double [] cof2 = {0,-2,1,7,-9};
-    double [] pow2 = {0,2,3,5,9};
+    int [] pow2 = {0,2,3,5,9};
     Polynomial p2 = new Polynomial(cof2, pow2);
 
     // add
     Polynomial s = p1.add(p2);
-    for (int i = 0; i < s.powers.length; i++){
-      System.out.print(s.coefficients[i] + "x^" + s.powers[i] + " + ");
-    }
-    System.out.println();
+    s.printPolynomial();
   
     // mltiply
     Polynomial mul = p1.multiply(p2);
-    for (int i = 0; i < mul.powers.length; i++){
-      System.out.print(mul.coefficients[i] + "x^" + mul.powers[i] + " + ");
-    }
-    System.out.println();
+    mul.printPolynomial();
+    
     
     // check root
     System.out.println("s(0.1) = " + s.evaluate(0.1));
@@ -29,5 +26,12 @@ public class Driver {
       System.out.println("1 is a root of s");
     else
       System.out.println("1 is not a root of s");
+
+    // open file
+    File myfile = new File("./poopoo.txt");
+    Polynomial p3 = new Polynomial(myfile);
+    p3.saveToFile("output.txt");
+    
+
   }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -1,27 +1,58 @@
 import java.lang.Math;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class Polynomial{
   public double coefficients[];
-  public double powers[];
+  public int powers[];
 
   public Polynomial(){
     this.coefficients = new double[]{0};
-    this.powers = new double[]{0};
+    this.powers = new int[]{0};
   }
   
-  public Polynomial(double coefficients[], double powers[]){
+  public Polynomial(double coefficients[], int powers[]){
     this.coefficients = coefficients;
     this.powers = powers;
+  }
+  
+  public Polynomial(File inputfile){
+    try {
+      Scanner scanner = new Scanner(inputfile);
+      if (scanner.hasNext()){
+        String currentline = scanner.nextLine();
+        // isolate each 
+        String numbergroups[] = currentline.split("[+-]");
+
+        this.coefficients = new double[numbergroups.length];
+        this.powers = new int[numbergroups.length];
+        for (int i = 0; i < numbergroups.length; i++){
+          if (numbergroups[i].length() == 1){
+            coefficients[i] = Double.parseDouble(numbergroups[i].substring(0,1));
+            powers[i] = 0;
+          }
+          else{
+            coefficients[i] = Double.parseDouble(numbergroups[i].substring(0,1));
+            powers[i] = Integer.parseInt(numbergroups[i].substring(2,3));
+          }
+        }
+      }
+    } catch (FileNotFoundException e){
+      e.printStackTrace();
+    }
   }
 
   public Polynomial add(Polynomial poly2){
     // create a new polynomial 
     // find the maximum length polynomial
-    double maxpow1 = this.powers[this.powers.length -1];
-    double maxpow2 = poly2.powers[poly2.powers.length -1];
-    double maxlen = Math.max(maxpow1, maxpow2)+1;
+    int maxpow1 = this.powers[this.powers.length -1];
+    int maxpow2 = poly2.powers[poly2.powers.length -1];
+    int maxlen = Math.max(maxpow1, maxpow2)+1;
     
-    Polynomial returnpoly = new Polynomial(new double[(int)maxlen], new double[(int)maxlen]);
+    Polynomial returnpoly = new Polynomial(new double[maxlen], new int[maxlen]);
     for (int i = 0; i < maxlen; i++){
       returnpoly.powers[i] = i;
       returnpoly.coefficients[i] = 0;
@@ -46,17 +77,45 @@ public class Polynomial{
     return evaluate(inputnum) == 0;
   }
   public Polynomial multiply(Polynomial poly2){
-    double maxpow1 = this.powers[this.powers.length -1];
-    double maxpow2 = poly2.powers[poly2.powers.length -1];
-    double maxlen = Math.max(maxpow1, maxpow2)+1;
+    int maxpow1 = this.powers[this.powers.length -1];
+    int maxpow2 = poly2.powers[poly2.powers.length -1];
+    int maxlen = Math.max(maxpow1, maxpow2)+1;
     
-    Polynomial returnpoly = new Polynomial(new double[(int)maxlen * 2], new double[(int)maxlen * 2]);
-    // multiple with 2 loops
+    Polynomial returnpoly = new Polynomial(new double[maxlen * 2], new int[maxlen * 2]);
+    // set powers
+    for (int i = 0; i < maxlen*2; i++){
+      returnpoly.powers[i] = i;
+      returnpoly.coefficients[i] = 0;
+    }
+    // multiply with 2 loops
     for (int i = 0; i < this.powers.length; i++){
       for (int j = 0; j < poly2.powers.length; j++){
-        returnpoly[this.powers[i] + poly2.powers[j]] += this.coefficients[i] * poly.coefficients[j];
+        returnpoly.coefficients[(int)this.powers[i] + (int)poly2.powers[j]] += this.coefficients[i] * poly2.coefficients[j];
       }
     }
     return returnpoly;
   }
+  public String createString(){
+    String retstr = "";
+    for (int i = 0; i < this.powers.length; i++){
+      retstr += (int)this.coefficients[i] + "x" + this.powers[i];
+      if (i != this.powers.length-1){
+        retstr += "+";
+      }
+    }
+    return retstr;
+  }
+  public void printPolynomial(){
+    System.out.println(createString());
+  }
+  public void saveToFile(String filename){
+    try {
+      FileWriter writer = new FileWriter(filename);
+      writer.write(createString()); 
+      writer.close();
+    } catch (IOException e){
+      System.err.println(e);
+    }
+  }
+
 }
