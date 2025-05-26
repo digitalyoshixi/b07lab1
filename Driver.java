e454f97cd51f67bcfad701b1929f51cb1189baf9
digitalyoshixi <digitalyoshixi@proton.me>
2025-05-26 12:01:52 -0400

    polynomial updates

--- Driver.java
+++ Driver.java
@@ -1,16 +1,33 @@
 public class Driver {
-public static void main(String [] args) {
-Polynomial p = new Polynomial();
-System.out.println(p.evaluate(3));
-double [] c1 = {6,0,0,5};
-Polynomial p1 = new Polynomial(c1);
-double [] c2 = {0,-2,0,0,-9};
-Polynomial p2 = new Polynomial(c2);
-Polynomial s = p1.add(p2);
-System.out.println("s(0.1) = " + s.evaluate(0.1));
-if(s.hasRoot(1))
-System.out.println("1 is a root of s");
-else
-System.out.println("1 is not a root of s");
-}
+  public static void main(String [] args) {
+    Polynomial p = new Polynomial();
+    System.out.println(p.evaluate(3));
+    double [] cof1 = {6,2,5};
+    double [] pow1 = {0,1,3};
+    Polynomial p1 = new Polynomial(cof1,pow1);
+    double [] cof2 = {0,-2,1,7,-9};
+    double [] pow2 = {0,2,3,5,9};
+    Polynomial p2 = new Polynomial(cof2, pow2);
+
+    // add
+    Polynomial s = p1.add(p2);
+    for (int i = 0; i < s.powers.length; i++){
+      System.out.print(s.coefficients[i] + "x^" + s.powers[i] + " + ");
+    }
+    System.out.println();
+  
+    // mltiply
+    Polynomial mul = p1.multiply(p2);
+    for (int i = 0; i < mul.powers.length; i++){
+      System.out.print(mul.coefficients[i] + "x^" + mul.powers[i] + " + ");
+    }
+    System.out.println();
+    
+    // check root
+    System.out.println("s(0.1) = " + s.evaluate(0.1));
+    if(s.hasRoot(1))
+      System.out.println("1 is a root of s");
+    else
+      System.out.println("1 is not a root of s");
+  }
 }
--- Polynomial.java
+++ Polynomial.java
@@ -2,31 +2,35 @@ import java.lang.Math;
 
 public class Polynomial{
   public double coefficients[];
+  public double powers[];
 
   public Polynomial(){
     this.coefficients = new double[]{0};
+    this.powers = new double[]{0};
   }
   
-  public Polynomial(double coefficients[]){
+  public Polynomial(double coefficients[], double powers[]){
     this.coefficients = coefficients;
+    this.powers = powers;
   }
 
   public Polynomial add(Polynomial poly2){
     // create a new polynomial 
     // find the maximum length polynomial
-    int max = poly2.coefficients.length;
-    if (this.coefficients.length > poly2.coefficients.length){
-      max = this.coefficients.length;
+    double maxpow1 = this.powers[this.powers.length -1];
+    double maxpow2 = poly2.powers[poly2.powers.length -1];
+    double maxlen = Math.max(maxpow1, maxpow2)+1;
+    
+    Polynomial returnpoly = new Polynomial(new double[(int)maxlen], new double[(int)maxlen]);
+    for (int i = 0; i < maxlen; i++){
+      returnpoly.powers[i] = i;
+      returnpoly.coefficients[i] = 0;
     }
-    Polynomial returnpoly = new Polynomial(new double[max]);
-    // sum new polynomial
-    for(int i = 0; i < max; i++){
-      if (i < this.coefficients.length){
-        returnpoly.coefficients[i] += this.coefficients[i];
-      }
-      if (i < poly2.coefficients.length){
-        returnpoly.coefficients[i] += poly2.coefficients[i];
-      }
+    for (int i = 0; i < this.powers.length; i++){
+      returnpoly.coefficients[(int)this.powers[i]] += this.coefficients[i];
+    }
+    for (int i = 0; i < poly2.powers.length; i++){
+      returnpoly.coefficients[(int)poly2.powers[i]] += poly2.coefficients[i];
     }
     // return new polynomial 
     return returnpoly;
@@ -34,11 +38,25 @@ public class Polynomial{
   public double evaluate(double inputnum){
     double returnsum = 0;
     for(int i = 0; i < this.coefficients.length; i++){
-      returnsum += this.coefficients[i] * Math.pow(inputnum,i);
+      returnsum += this.coefficients[i] * this.powers[i];
     }
     return returnsum;
   }
   public boolean hasRoot(double inputnum){
     return evaluate(inputnum) == 0;
+  }
+  public Polynomial multiply(Polynomial poly2){
+    double maxpow1 = this.powers[this.powers.length -1];
+    double maxpow2 = poly2.powers[poly2.powers.length -1];
+    double maxlen = Math.max(maxpow1, maxpow2)+1;
+    
+    Polynomial returnpoly = new Polynomial(new double[(int)maxlen * 2], new double[(int)maxlen * 2]);
+    // multiple with 2 loops
+    for (int i = 0; i < this.powers.length; i++){
+      for (int j = 0; j < poly2.powers.length; j++){
+        returnpoly[this.powers[i] + poly2.powers[j]] += this.coefficients[i] * poly.coefficients[j];
+      }
+    }
+    return returnpoly;
   }
 }
